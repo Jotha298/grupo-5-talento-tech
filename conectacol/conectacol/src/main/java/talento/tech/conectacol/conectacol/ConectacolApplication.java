06f6f0210e09ddbf1f88e358581384151b774ec2
quintana29 <jhonatanquintana29@gmail.com>
2024-11-22 20:44:18 -0500

    Subiendo proyecto

---
+++ conectacol/conectacol/src/main/java/talento/tech/conectacol/conectacol/ConectacolApplication.java
@@ -0,0 +1,13 @@
+package talento.tech.conectacol.conectacol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectacolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectacolApplication.class, args);
+	}
+
+}
